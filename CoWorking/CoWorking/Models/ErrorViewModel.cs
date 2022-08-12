bca57fd0300c02f50504679fd382c652645dd998
gerardo lanza <glanza007@gmail.com>
2022-08-12 08:38:57 -0600

    First Commit

---
+++ CoWorking/CoWorking/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoWorking.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
